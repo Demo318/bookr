a24596813d545eca918c7adf89e65376ce79805a
Devin Mork <devin.mork@gmail.com>
2024-03-23 11:20:16 -0500

    Complete Exercise 1.07

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 def index(request):
-    name = request.GET.get("name") or "world"
-    return render(request, "base.html")
+    name = "world"
+    return render(request, "base.html", {"name": name})
