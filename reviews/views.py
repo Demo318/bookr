102ae72b5053b12b61dcf741382439e888d2184a
Devin Mork <devin.mork@gmail.com>
2024-03-23 16:29:05 -0500

    Complete Chapter 1

--- reviews/views.py
+++ reviews/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def index(request):
     name = "world"
     return render(request, "base.html", {"name": name})
+
+def search(request):
+    search_query = request.GET.get("search-query") or ""
+    return render(request, "search_result.html", {"search_query": search_query})
