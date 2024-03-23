51a6ea8ae86164d2fe24ade91482b32c45e6c4b2
Devin Mork <devin.mork@gmail.com>
2024-03-23 11:06:02 -0500

    Complete Exercise 1.04

--- reviews/views.py
+++ reviews/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    name = request.GET.get("name") or "world"
+    return HttpResponse("Hello, {}!".format(name))
