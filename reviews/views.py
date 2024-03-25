b6183c28dd1701a0dc9ff0704eaeebe8fca0bd64
Devin Mork <devin.mork@gmail.com>
2024-03-25 15:48:56 -0500

    Complete Exercise 3.01

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
 
 def index(request):
     name = "world"
@@ -7,3 +9,7 @@ def index(request):
 def search(request):
     search_query = request.GET.get("search-query") or ""
     return render(request, "search_result.html", {"search_query": search_query})
+
+def welcome_view(request):
+    message = f"<html><h1>Welcome to Bookr!</h1><p>{Book.objects.count()} books and counting!</p></html>"
+    return HttpResponse(message)
