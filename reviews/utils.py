4dddceae5eabdf72f4e250f44d000d3011335125
Devin Mork <devin.mork@gmail.com>
2024-03-26 09:03:37 -0500

    Complete Chapter 3

--- reviews/utils.py
+++ reviews/utils.py
@@ -1,4 +1,16 @@
 def average_rating(rating_list):
+    """Provides average rating of book reviews."""
     if not rating_list:
         return 0
     return round(sum(rating_list) / len(rating_list))
+
+def evaluate_reviews(book, reviews=None):
+    """Retrieves reviews associated with book."""
+    if not reviews:
+        reviews = book.review_set.all()
+    if reviews:
+        return (
+            average_rating([review.rating for review in reviews]),
+            len(reviews)
+        )
+    return ( None, 0 )
--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Book
-from .utils import average_rating
+from .models import Book, Review
+from .utils import average_rating, evaluate_reviews
 
 def index(request):
     return render(request, 'base.html')
@@ -14,22 +14,23 @@ def book_list(request):
     books = Book.objects.all()
     book_list = []
     for book in books:
-        reviews = book.review_set.all()
-        if reviews:
-            book_rating = average_rating([review.rating for review in reviews])
-            number_of_reviews = len(reviews)
-        else:
-            book_rating = None
-            number_of_reviews = 0
+        reviews_stats = evaluate_reviews(book)
         book_list.append({
             'book': book,
-            'book_rating': book_rating,
-            'number_of_reviews': number_of_reviews
+            'book_rating': reviews_stats[0],
+            'number_of_reviews': reviews_stats[1]
         })
     context = {'book_list': book_list}
     return render(request, 'reviews/book_list.html', context)
 
 def book_detail(request, pk):
     book = Book.objects.get(pk=pk)
-    # reviews = Review.
+
+    reviews = Review.objects.filter(book=book)
+    reviews_stats = evaluate_reviews(book, reviews)
+
+    book.book_rating = reviews_stats[0]
+    context = { 'book': book, 'reviews': reviews }
+    return render(request, 'reviews/book_detail.html', context)
+
 
