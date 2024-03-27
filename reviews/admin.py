e384cebf2cd493e411c10165ad032da9ba682b09
Devin Mork <devin.mork@gmail.com>
2024-03-27 09:21:37 -0500

    Create custom admin site subclass.

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib.admin import AdminSite
 from reviews.models import (
     Publisher,
     Contributor,
@@ -7,8 +7,15 @@ from reviews.models import (
     Review
 )
 
-admin.site.register(Publisher)
-admin.site.register(Contributor)
-admin.site.register(Book)
-admin.site.register(BookContributor)
-admin.site.register(Review)
+class BookrAdminSite(AdminSite):
+    title_header = 'Bookr Admin'
+    site_header = 'Bookr administration'
+    index_title = 'Bookr site admin'
+
+admin_site = BookrAdminSite(name='bookr')
+
+admin_site.register(Publisher)
+admin_site.register(Contributor)
+admin_site.register(Book)
+admin_site.register(BookContributor)
+admin_site.register(Review)
