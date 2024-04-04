b0e5d5c5d23883092a0ca09b6db804030a66b7eb
Devin Mork <devin.mork@gmail.com>
2024-04-04 11:39:05 -0500

    Complete Exercise 11.03

---
+++ filter_demo/templatetags/inclusion_tag.py
@@ -0,0 +1,7 @@
+from django import template
+register = template.Library()
+
+@register.inclusion_tag('book_list.html')
+def book_list(books):
+    book_list = [book_name for book_name, book_autho in books.items()]
+    return {'book_list': book_list}
