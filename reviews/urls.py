5f735b617e33523b6c7ab237195385b2ff0edfb5
Devin Mork <devin.mork@gmail.com>
2024-04-05 17:16:47 -0500

    Use a router for the API!

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,10 +1,14 @@
 # from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views, api_views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'books', api_views.BookViewSet)
+router.register(r'reviews', api_views.ReviewViewSet)
 
 urlpatterns = [
-    path('api/all-books/', api_views.AllBooks.as_view(), name='all_books'),
-    path('api/contributors/', api_views.ContributorView.as_view(), name='contributors'),
+    path('api/', include((router.urls, 'api'))),
     path('books/', views.book_list, name='book_list'),
     path('book/<pk>/', views.book_detail, name='book_detail'),
     path('books/<int:pk>/media/', views.book_media, name='book_media'),
