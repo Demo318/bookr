a0acd8672659c859685a7d2e1599011a63343f3b
Devin Mork <devin.mork@gmail.com>
2024-04-06 15:07:39 -0500

    Complete Exercise 14.04

--- bookr_test/tests.py
+++ bookr_test/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 from .models import Publisher
+from django.contrib.auth.models import User
 
 class TestPublisherModel(TestCase):
     """Test the publisher model."""
@@ -23,4 +24,26 @@ class TestGreetingView(TestCase):
     
     def test_greeting_view(self):
         response = self.client.get('/test/greeting')
+        self.assertEqual(response.status_code, 200)
+
+class TestLoggedInGreetingView(TestCase):
+    """Test the greeting view for the authenticaed users."""
+    def setUp(self):
+        test_user = User.objects.create_user(
+            username='testuser', 
+            password='test@#628password'
+        )
+        test_user.save()
+        self.client = Client()
+
+    def test_user_greeting_not_authenticated(self):
+        response = self.client.get('/test/greet_user')
+        self.assertEqual(response.status_code, 302)
+
+    def test_user_authenticated(self):
+        login = self.client.login(
+            username='testuser',
+            password='test@#628password'
+        )
+        response = self.client.get('/test/greet_user')
         self.assertEqual(response.status_code, 200)
